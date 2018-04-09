ee54fe1e1e5fcb5957c80a51f5cbe210b5dd9f93
k10l27r <37723467+k10l27r@users.noreply.github.com>
2018-04-09 22:43:22 +0700

    08ю04ю2018

--- web/js/script.js
+++ web/js/script.js
@@ -1,16 +1,25 @@
 jQuery(document).ready(function() {
+
+   if(($('body').css('overflow') === 'hidden') &&
+      ($('div.container').css('width') === '950px'))
+   {
+      $('body').removeAttr('style');
+   }
+   else {
+     $('body').css({"overflow":"hidden"});
+   }
   $('.menuToggle').click(function() {
+    if($('body').css('overflow') === 'hidden')
+    {
+       $('body').removeAttr('style');
+    }
+    else {
+      $('body').css({"overflow":"hidden"});
+    }
     $('nav').slideToggle(500,function(){
       if($(this).css('display') === 'none')
       {
          $(this).removeAttr('style');
-      }
-      if($('body').css('overflow') === 'hidden')
-      {
-         $('body').removeAttr('style');
-      }
-      else {
-        $('body').css({"overflow":"hidden"});
       }
 
     });
