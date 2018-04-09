af138bd0e2780f7462026bc4eed8ed31006ceee5
k10l27r <37723467+k10l27r@users.noreply.github.com>
2018-04-09 22:10:01 +0700

    09.04.2018

--- web/js/script.js
+++ web/js/script.js
@@ -1,9 +1,18 @@
 jQuery(document).ready(function() {
   $('.menuToggle').click(function() {
-    $('.menu').slideToggle(500,function(){
-      if($(this).css('display') === 'none'){
+    $('nav').slideToggle(500,function(){
+      if($(this).css('display') === 'none')
+      {
          $(this).removeAttr('style');
       }
+      if($('body').css('overflow') === 'hidden')
+      {
+         $('body').removeAttr('style');
+      }
+      else {
+        $('body').css({"overflow":"hidden"});
+      }
+
     });
 });
 });
